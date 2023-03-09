a98d08cbe69f659fe6cdccb327a3e2148ea39213
yintingwen <yintingwen@foxmail.com>
2023-03-09 17:37:35 +0800

    项目初始结构创建

---
+++ order/src/test/java/com/ytw/maill/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ytw.maill.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
